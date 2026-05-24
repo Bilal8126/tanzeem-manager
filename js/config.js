75e1858198b616f97e61055f5139f3b0a9411b3c
Bilal8126 <erhacker81@gmail.com>
2026-05-24 14:36:17 +0530

    123Updated

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   CLIENT_ID:   '627419494712-e7n7t9dp806aeheqhiroe5s8t6jmhf94.apps.googleusercontent.com',
   SHEET_ID:    '1UOJBM4_pnlZiO4oiENTr5vrO3F2CHCsrsefk-TzMpH4',
   SCOPES:      'https://www.googleapis.com/auth/spreadsheets.readonly',
-  WORKER_URL:  'https://tanzeem-proxy.YOUR_SUBDOMAIN.workers.dev',
+  WORKER_URL:  'https://tanzeem-manager.erhacker81.workers.dev',
   SESSIONS: [
     { label: '2025-26', sheet: 'Session (2025-26)', expenses: 'Expense List (25-26)', donations: 'Donation (25-26)' },
     { label: '2024-25', sheet: 'Session (2024-25)', expenses: 'Expenses',             donations: 'Donations'         },
